0b26329fd5435b598e5c9aa2a97dffa22e979d83
donnymays <dtmays2@gmail.com>
2020-08-12 14:53:45 -0700

    initial commit build html and js
    co-authored-by: Natalie Murphy <nataliemurphy500@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,20 @@
-
-function add(number1, number2) {
+const add = function(number1, number2) {
   return number1 + number2;
-  }
-  
-  const number1 = parseInt(prompt("Enter a number:"));
-  alert(number1);
-  const number2 = parseInt(prompt("Enter another number"));
-  
-  alert(add(number1, number2));
-  
+};
+
+const subtract = function(number1, number2) {
+  return number1 - number2;
+};
+
+const multiply = function(number1, number2) {
+  return number1 * number2;
+};
+
+const divide = function(number1, number2) {
+  return number1 / number2;
+};
+
+const number1 = parseInt(prompt("Enter a number:"));
+const number2 = parseInt(prompt("Enter another number:"));
+
+alert(add(number1, number2));
